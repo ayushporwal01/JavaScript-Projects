2c64b7597601e195b9404a9d008babdac12ef263
ayushporwal01 <porwalayush953@gmail.com>
2025-12-15 15:23:13 +0530

    Initial Commit

--- 01 - Counter App/script.js
+++ 01 - Counter App/script.js
@@ -1,1 +1,18 @@
+let counter = document.getElementById('counter');
+let incrementBtn = document.getElementById('increment-btn');
+let decrementBtn = document.getElementById('decrement-btn');
+
+let currValue = parseInt(counter.value);
+
+incrementBtn.addEventListener('click', (e) => {
+    currValue += 1;
+    counter.value = currValue;
+})
+
+decrementBtn.addEventListener('click', (e) => {
+    if(currValue > 0) {
+       currValue -= 1;
+       counter.value = currValue;
+    }
+})
 
