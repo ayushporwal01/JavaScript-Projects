7dadf831fe0d468e1c34c571d0da30007d945d7b
ayushporwal01 <porwalayush953@gmail.com>
2025-12-16 11:03:15 +0530

    Initial Commit

--- 02 - Background Color Changer/script.js
+++ 02 - Background Color Changer/script.js
@@ -2,7 +2,14 @@ const changeBtn = document.getElementById('change-btn');
 
 const hex = '#';
 
+//change on button click
 changeBtn.addEventListener('click', (e) => {
     let randomColor = "#" + (Math.random().toString(16).slice(2, 8));
     document.body.style.backgroundColor = randomColor;
 })
+
+// change automatically
+setInterval((e) => {
+    let randomColor = "#" + (Math.random().toString(16).slice(2, 8));
+    document.body.style.backgroundColor = randomColor;
+}, 1000);
