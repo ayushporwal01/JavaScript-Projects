6e11ecf8be7c2c19b36a13a891ff8e0e49053da0
ayushporwal01 <porwalayush953@gmail.com>
2025-12-18 22:39:21 +0530

    Initial Commit

--- 04 - Snake Game/script.js
+++ 04 - Snake Game/script.js
@@ -1,3 +1,4 @@
 const playBoard = document.querySelector('.play-board');
 const scoreElement = document.querySelector('.score');
 const highScoreElement = document.querySelector('.high-score');
+const controls = document.querySelector('.controls');
