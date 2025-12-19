c8ea600dbdf7f6f785b969a2a20e70923480265e
ayushporwal01 <porwalayush953@gmail.com>
2025-12-19 10:59:52 +0530

    Initial Commit

--- 04 - Snake Game/script.js
+++ 04 - Snake Game/script.js
@@ -78,4 +78,12 @@ const initGame = () => {
     for(let i = snakeBody.length - 1; i > 0; i--) {
         snakeBody[i] = snakeBody[i - 1];
     }
+
+    snakeBody[0] = [snakeX, snakeY];
+
+    //Check snake body is out of wall or not
+
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+       return gameOver = true;
+    }
 }
