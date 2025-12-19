2143a1832360d968884f92661ffb22b3cc6caa9a
ayushporwal01 <porwalayush953@gmail.com>
2025-12-19 11:20:46 +0530

    Initial Commit

--- 04 - Snake Game/script.js
+++ 04 - Snake Game/script.js
@@ -32,20 +32,20 @@ const handleGameOver = () => {
 //Change velocity value based on key press
 
 const changeDirection = (e) => {
-    if(e.key === "ArrowUp" && velocityY != 1) {
-       velocityX = 0;
-       velocityY = -1;
-    } else if(e.key === "ArrowDown" && velocityY != -1) {
+    if (e.key === "ArrowUp" || e.key === "w" && velocityY !== 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.key === "ArrowDown" || e.key === "s" && velocityY !== -1) {
         velocityX = 0;
         velocityY = 1;
-    } else if(e.key === "ArrowLeft" && velocityX != 1) {
+    } else if (e.key === "ArrowLeft" || e.key === "a" && velocityX !== 1) {
         velocityX = -1;
         velocityY = 0;
-    } else if(e.key === "ArrowRight" && velocityX != -1) {
+    } else if (e.key === "ArrowRight" || e.key === "d" && velocityX !== -1) {
         velocityX = 1;
         velocityY = 0;
     }
-}
+};
 
 // Change direction on each key click
 
