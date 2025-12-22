e74250dae7e5b6f149911a6874fe7209674a5fe3
ayushporwal01 <porwalayush953@gmail.com>
2025-12-22 12:00:45 +0530

    Initial Commit

--- 05 - Tic Tac Toe/script.js
+++ 05 - Tic Tac Toe/script.js
@@ -34,6 +34,10 @@ const checkWinner = () => {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            
+        }
     }
 }
 
