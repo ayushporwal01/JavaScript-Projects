a3486bd5e1595b59a0fa35d2467f2065df70f322
ayushporwal01 <porwalayush953@gmail.com>
2025-12-22 11:26:17 +0530

    Initial Commit

--- 05 - Tic Tac Toe/script.js
+++ 05 - Tic Tac Toe/script.js
@@ -1,5 +1,18 @@
 let boxes = document.querySelectorAll('.box');
 let resetBtn = document.querySelector('#reset-btn');
 
+let turnO = true;
+
+let winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
 
 
