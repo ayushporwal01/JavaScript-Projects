7c3bff805a97eace1268be7c0c1e5f119579074c
ayushporwal01 <porwalayush953@gmail.com>
2025-12-24 11:40:28 +0530

    Initial Commit

--- 06 - Flappy Bird/flappybird.js
+++ 06 - Flappy Bird/flappybird.js
@@ -3,3 +3,10 @@ let board;
 let boardWidth = 360;
 let boardHeight = 640;
 let context;
+
+window.onload(() => {
+    board = document.getElementById('board');
+    board.height = boardHeight;
+    board.width = boardWidth;
+    board.context = context;
+})
