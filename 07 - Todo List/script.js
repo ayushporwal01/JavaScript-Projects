bc66464e5545ac0f06b2bd8f54c6d7fd5e32d530
ayushporwal01 <porwalayush953@gmail.com>
2025-12-25 23:03:16 +0530

    Initial Commit

--- 07 - Todo List/script.js
+++ 07 - Todo List/script.js
@@ -1,3 +1,7 @@
 let taskInput = document.getElementById('task-input');
 let addBtn = document.getElementById('add-btn');
 let taskList = document.getElementById('task-list');
+
+//Load Tasks
+let saved = localStorage.getItem('tasks');
+let tasks = saved ? JSON.parse(saved) : [];
