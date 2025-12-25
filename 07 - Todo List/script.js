2434ea35561ee212eba1be6e2455be8a22f29409
ayushporwal01 <porwalayush953@gmail.com>
2025-12-25 23:27:34 +0530

    Initial Commit

--- 07 - Todo List/script.js
+++ 07 - Todo List/script.js
@@ -16,7 +16,7 @@ function createTasks(task, index) {
     //checkbox to toggle completion
     const checkbox = docuement.createElement('input');
     checkbox.type = 'checkbox';
-    checkbox.checked = !!task.completed;
+    checkbox.checked = !!task.completed; //convert to true or false
     checkbox.addEventListener("change", () => {
         todo.completed = checkbox.checked;
 
