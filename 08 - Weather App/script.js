60e8a15e02e516d46a981a3574ffaf5690710569
ayushporwal01 <porwalayush953@gmail.com>
2025-12-28 12:05:23 +0530

    Initial Commit

---
+++ 08 - Weather App/script.js
@@ -0,0 +1,6 @@
+let searchBar = document.querySelector('#search-bar');
+let searchBtn = document.querySelector('#search-btn');
+let temp = document.querySelector('.temp');
+let city = document.querySelector('.city');
+let humidity = document.querySelector('.humidity');
+let wind = document.querySelector('.wind');
