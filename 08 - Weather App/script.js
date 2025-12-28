439760867846b81553bdfa7626a711c6d2dac750
ayushporwal01 <porwalayush953@gmail.com>
2025-12-28 12:12:29 +0530

    Initial Commit

--- 08 - Weather App/script.js
+++ 08 - Weather App/script.js
@@ -6,3 +6,5 @@ let city = document.querySelector('.city');
 let humidity = document.querySelector('.humidity');
 let wind = document.querySelector('.wind');
 
+const APIKEY = "5946f5b03d140b69d87678908bea2ee0";
+const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`; 
