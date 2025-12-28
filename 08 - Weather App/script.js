16fe8673367a00bef26e8e1bf58cec64c7a76be5
ayushporwal01 <porwalayush953@gmail.com>
2025-12-28 12:28:53 +0530

    Initial Commit

--- 08 - Weather App/script.js
+++ 08 - Weather App/script.js
@@ -3,10 +3,16 @@ let searchBtn = document.querySelector('#search-btn');
 let weatherImg = document.querySelector('#weather-img');
 
 const APIKEY = "5946f5b03d140b69d87678908bea2ee0";
-const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`; 
 
 async function checkWeather(city = 'Indore') {
-    const res = await fetch(URL + city + `&appid=${APIKEY}`);
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`; 
+    const res = await fetch(URL);
+
+    if(!res.ok) {
+       alert("City not found, please try again!");
+       return;
+    }
+    
     let data = await res.json();
     document.querySelector('.city').innerHTML = data.name;
     document.querySelector('.temp').innerHTML = Math.floor(data.main.temp) + "°C";
